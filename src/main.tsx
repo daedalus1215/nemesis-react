19b45b145e64f2ffff7ca8946b0bccf2c8a5eb9a
la la <13687739+daedalus1215@users.noreply.github.com>
2024-12-15 21:27:29 -0500

    setup and integrate with cognito

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { AuthProvider } from "react-oidc-context";
+import './index.css'
+import App from './App.tsx'
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>,
+)
