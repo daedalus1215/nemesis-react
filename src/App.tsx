c14b2118e724164f144b96ef913adfa139494056
la la <13687739+daedalus1215@users.noreply.github.com>
2024-12-22 20:20:57 -0500

    friends: Add more routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page/HomePage";
+import FriendsPage from "./pages/friends-page/FriendsPage";
+import LoginPage from "./pages/login-page/LoginPage";
+import styles from "./App.module.css";
 import AuthGuard from "./auth/AuthGuard";
-import HomePage from './pages/home-page/HomePage';
-
 
 export function App() {
   return (
     <Router>
+      <header>
+        <nav>
+          <div className={styles["title"]}>Nemesis</div>
+        </nav>
+      </header>
       <Routes>
-        {/* <Route element={<AuthGuard />}> */}
-          <Route path="/" element={<HomePage />} />          
-        {/* </Route> */}
+        <Route path="/" element={<LoginPage />} />
+        <Route element={<AuthGuard />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/friends" element={<FriendsPage />} />
+        </Route>
       </Routes>
     </Router>
   );
