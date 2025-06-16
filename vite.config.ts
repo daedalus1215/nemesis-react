130e680bf145481dc498b5298e6c55a535e6132a
la la <13687739+daedalus1215@users.noreply.github.com>
2025-06-16 00:06:38 -0400

    add allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { env } from './vite.env.config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [env.VITE_ALLOWED_HOSTS],
+  },
 })
