81f0c8a61651f187df49daf944decb5e75e14709
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-01-09 23:37:59 -0500

    update copy when oweing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1clcno8pfrs"
+    "revision": "0.qv4jhv3d928"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
